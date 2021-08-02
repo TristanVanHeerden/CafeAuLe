20f7a83aa65fe2117d51577ebb0a3b679274bf31
Tristan van Heerden <tj011504@gmail.com>
2021-08-02 11:52:26 +0800

    Add project files.

---
+++ CafeAuLe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CafeAuLe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+top:
+            Console.WriteLine(@"Welcome to cafe au le
+Please select your operation.");
+            Console.WriteLine("1 : Order");
+            Console.WriteLine("2 : Summery");
+            Console.ReadLine();
+            int input = 1;
+            if (input < 2) ;
+        }
+    }
+}
